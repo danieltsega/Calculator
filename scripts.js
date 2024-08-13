24dba59915dd1b59a8c49e3eabf6ab98e7ea0bd8
danieltsega <danieltsega6658@gmail.com>
2024-08-13 09:35:20 +0300

    feat: test out the basic arithmetic functions

---
+++ scripts.js
@@ -0,0 +1,28 @@
+// Addition Function
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+// Substraction Function
+function substract(num1, num2) {
+  return num1 - num2;
+}
+
+// Multiply Function
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+// Divide Function
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+const num1 = 5;
+const num2 = 5;
+const operator = "";
+
+console.log(add(num1, num2));
+console.log(substract(num1, num2));
+console.log(multiply(num1, num2));
+console.log(divide(num1, num2));
